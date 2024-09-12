aa6f713f9444f7bbfc89d6e42f2e3afaf9da73c3
yadrielsamir <yadrielsamir@gmail.com>
2024-09-12 02:03:06 -0400

    Changed bottom icon

--- safe-rum/app/(tabs)/_layout.tsx
+++ safe-rum/app/(tabs)/_layout.tsx
@@ -1,11 +1,18 @@
 import { Tabs } from "expo-router"
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default () => {
     return(
         <Tabs>
             <Tabs.Screen name="home" />
             <Tabs.Screen name="ReportMap" />
-            <Tabs.Screen name="AdminProfile" />
+            <Tabs.Screen name="Profile" 
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="person" color={color} size={size} /> 
+                ),
+              }}
+            />
         </Tabs>
     )
 }
