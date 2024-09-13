1678b70feb256d6518cc4c730d6f5bfd846c82b9
William A. Martínez Martínez <william.martinez10@upr.edu>
2024-09-13 11:22:25 -0400

    Revert "Merge branch 'main' into userAuthenticationUIComponents"
    This reverts commit d6a1af826cf94becd2263cbec9c294779346b4f7, reversing
    changes made to b8a4a64e8cc739ab9f8a8fc1c5a2e27f9248ac09.

--- safe-rum/app/(tabs)/index.tsx
+++ safe-rum/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello World!</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
