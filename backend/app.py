f108fcc948d1c45961419899422088311d32c780
joel-alvarado <joel.alvarado2@upr.edu>
2024-10-01 20:56:14 -0400

    revert app factory changes

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,25 @@
 from blueprints.admins import admins_bp
 from blueprints.alerts import alerts_bp
 
+from blueprints.auth import auth_bp
 from blueprints.reports import reports_bp
 from blueprints.users import users_bp
 from config import Config
+from flask import Flask
+from flask_mail import Mail
 from models import db
 
-from backend import Flask
+mail = Mail()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    mail.init_app(app)
 
     # Register blueprints
+    app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(reports_bp, url_prefix="/reports")
     app.register_blueprint(alerts_bp, url_prefix="/alerts")
     app.register_blueprint(users_bp, url_prefix="/user")
