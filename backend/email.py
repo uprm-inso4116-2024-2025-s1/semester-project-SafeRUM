3baa03510ccbd9353708d2bf45ed01f561d1fa4e
joel-alvarado <joel.alvarado2@upr.edu>
2024-10-01 20:08:15 -0400

    apply formatting to email.py using black formatter

--- backend/email.py
+++ backend/email.py
@@ -13,5 +13,6 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email,
-           args=(current_app._get_current_object(), msg)).start()
+    Thread(
+        target=send_async_email, args=(current_app._get_current_object(), msg)
+    ).start()
