e37b9ad49cb11760993bcb0058a7659cc0c85574
joel-alvarado <joel.alvarado2@upr.edu>
2024-10-01 21:54:51 -0400

    added send_verification_email method

--- backend/email.py
+++ backend/email.py
@@ -1,7 +1,9 @@
 from threading import Thread
+
+from app import mail
+from config import Config
 from flask import current_app
 from flask_mail import Message
-from app import mail
 
 
 def send_async_email(app, msg):
@@ -12,7 +14,20 @@ def send_async_email(app, msg):
 def send_email(subject, sender, recipients, text_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
-    msg.html = html_body
     Thread(
         target=send_async_email, args=(current_app._get_current_object(), msg)
     ).start()
+
+
+def send_verification_email(user, token):
+    verification_url = current_app.url_for("verify", token=token)
+    subject = "[SafeRUM] Verify Your Email Address"
+    recipients = [user.email]
+    body = f"Hi {user.first_name}, please click the following link to verify your email: {verification_url}"
+
+    send_email(
+        subject=subject,
+        sender=Config.MAIL_DEFAULT_SENDER,
+        recipients=recipients,
+        text_body=body,
+    )
