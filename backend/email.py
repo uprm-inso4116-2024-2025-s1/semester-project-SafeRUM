1bcbb95e231abf279cad23e26cfabff611982b81
joel-alvarado <joel.alvarado2@upr.edu>
2024-10-01 21:42:39 -0400

    remove html arg from send_email

--- backend/email.py
+++ backend/email.py
@@ -9,7 +9,7 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(subject, sender, recipients, text_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
