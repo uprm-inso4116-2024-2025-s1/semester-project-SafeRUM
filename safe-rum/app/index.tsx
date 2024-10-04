7c634bb4676f20c042043781a484af120c8c0f61
William A. Martínez Martínez <william.martinez10@upr.edu>
2024-10-04 11:08:45 -0400

    Set role selection as initial screen on app launch

--- safe-rum/app/index.tsx
+++ safe-rum/app/index.tsx
@@ -1,8 +1,9 @@
 import { Redirect } from "expo-router";
+import React = require("react");
 
 // App starts here
 const StartPage = () => {
-  return <Redirect href="/home" />;
+  return <Redirect href="/RoleSelection" />;
 };
 
 export default StartPage;
