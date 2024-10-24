bba7d922226aae29d7de8f3bd6cf735973e9927b
RaquelQuinones <raquel.quinones7@upr.edu>
2024-10-23 20:05:13 -0400

    Implemented Redirection System

--- safe-rum/components/UserLogin.tsx
+++ safe-rum/components/UserLogin.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, TextInput, TouchableOpacity, Text, View, Alert, Modal } fro
 import { Ionicons } from '@expo/vector-icons';
 import { initializeApp } from '@firebase/app';
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from '@firebase/auth';
+import { useNavigation } from '@react-navigation/native'; // For navigation
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCIb-bHGc68LhhHOGmz5QjZBJ5T3DAoGO4",
@@ -16,6 +18,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const navigation = useNavigation(); // Use navigation for redirection
+
 
 interface UserLoginScreenProps {
   toggleUserAuthScreen: () => void;
@@ -65,6 +69,15 @@ export default function UserLogin({ toggleUserAuthScreen }: UserLoginScreenProps
         setUserAuthenticated(true); 
         Alert.alert('Success', 'You have logged in successfully!');
         clearLogInItems();
+
+        const userRole = 'admin'; 
+
+        if (userRole === 'admin') {
+          navigation.navigate('AdminDashboard'); // Redirect to Admin dashboard (this need to change to the dashboard when that task is done)
+        } else {
+          navigation.navigate('home'); // Redirect regular users to Home
+        }
+
       })
       .catch((error) => {
         Alert.alert('Authentication Error', error.message);
